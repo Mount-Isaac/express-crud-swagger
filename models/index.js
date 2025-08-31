1c1b295e3a77fbae01b5dadf49e5e240dbc5f194
isaac kyalo <kyalomutie@students.ku.ac.ke>
2025-09-01 01:15:22 +0300

    db connection with URI

--- models/index.js
+++ models/index.js
@@ -1,13 +1,32 @@
 import { Sequelize } from "sequelize";
 
+// export const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialect: "mysql",
+//         logging: false
+//     }
+// )
+
+// mysql://db_user:db_password@db_host:db_port/db_name
+const dbName = process.env.DB_NAME
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
 export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "mysql",
-        logging: false
+    `mysql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`, 
+    { 
+        logging: false, 
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
     }
 )
